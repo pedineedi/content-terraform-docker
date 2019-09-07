9a69cfc47b40f14a93950c81f270c06389f2a274
pedineedi <pedineedi@gmail.com>
2019-09-07 13:33:24 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "docker_image" "image_id" {
 
 # Start the Container
 resource "docker_container" "container_id" {
-  name  = "${var.container_name}"
+  name  = "ghost_blog_name"
   image = "${docker_image.image_id.latest}"
   ports {
     internal = "${var.int_port}"
